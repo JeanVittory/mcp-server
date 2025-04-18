8a1df29960efd0beffd59ccdbb88a452f4d6e09a
JeanVittory <vittory.dev@gmail.com>
2025-04-18 17:52:12 -0500

    feat: :art: Add constants file with docs_urls, serper_url and user_agent variable

---
+++ src/constants/constants.py
@@ -0,0 +1,7 @@
+USER_AGENT="docs-app/1.0"
+SERPER_URL="google.serper.dev"
+docs_urls={
+    "langchain": "python.langchain.com/docs",
+    "llama-index": "docs.llamaindex.ai/en/stable",
+    "openai": "platform.openai.com/docs"
+}
